c7d518a8e90b9a64546d4ce7553d4a02e86fee37
chabbi.milind@gmail.com <chabbi.milind@gmail.com@cdcf3bf0-21c1-d580-df87-83d91baef4e5>
2014-03-07 02:36:00 +0000

    moving to configure based build

--- trunk/build.sh
+++ trunk/build.sh
@@ -2,10 +2,9 @@ set -ex
 PATH_TO_GOOGLE_SPARSE_HASH=/projects/hpc/mc29/software/sparsehash-2.0.2
 PATH_TO_PIN=/projects/hpc/mc29/software/pin_rev/pin-2.13-62732-gcc.4.4.7-linux/
 PATH_TO_BOOST=/projects/pkgs/boost_1_47_0/
-#uncomment if you want to clean before building
-#make SPARSEHASH_PATH=$PATH_TO_GOOGLE_SPARSE_HASH PIN_PATH=$PATH_TO_PIN BOOST_PATH=$PATH_TO_BOOST clean
-make SPARSEHASH_PATH=$PATH_TO_GOOGLE_SPARSE_HASH PIN_PATH=$PATH_TO_PIN BOOST_PATH=$PATH_TO_BOOST
-#uncomment if you want to test your build
-#make SPARSEHASH_PATH=$PATH_TO_GOOGLE_SPARSE_HASH PIN_PATH=$PATH_TO_PIN BOOST_PATH=$PATH_TO_BOOST check
+./configure --with-Pin=$PATH_TO_PIN --with-boost=$PATH_TO_BOOST --with-sparse-hash=$PATH_TO_GOOGLE_SPARSE_HASH --enable-develop
+make
+# uncomment to run sanity tests
+#make check
 echo "*********SUCCESS***********"
 
