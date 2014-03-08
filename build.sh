290a20e7f6de4c80f489112abb405dd97c5844a0
chabbi.milind <chabbi.milind@gmail.com>
2014-03-08 05:53:29 +0000

    Using UpdateCurTraceXXX functions to set current trace and ipnode values. Enforced CALL_ORDER_LAST for CCTLib analysis routine for RET INS. Added assertion checks for GetContextXXX functions. Disabled debug mode for default builds

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@ set -ex
 PATH_TO_GOOGLE_SPARSE_HASH=/projects/hpc/mc29/software/sparsehash-2.0.2
 PATH_TO_PIN=/projects/hpc/mc29/software/pin_rev/pin-2.13-62732-gcc.4.4.7-linux/
 PATH_TO_BOOST=/projects/pkgs/boost_1_47_0/
-./configure --with-Pin=$PATH_TO_PIN --with-boost=$PATH_TO_BOOST --with-sparse-hash=$PATH_TO_GOOGLE_SPARSE_HASH --enable-develop
+#develop is off by default
+#./configure --with-Pin=$PATH_TO_PIN --with-boost=$PATH_TO_BOOST --with-sparse-hash=$PATH_TO_GOOGLE_SPARSE_HASH --enable-develop
+./configure --with-Pin=$PATH_TO_PIN --with-boost=$PATH_TO_BOOST --with-sparse-hash=$PATH_TO_GOOGLE_SPARSE_HASH 
 make
 # uncomment to run sanity tests
 #make check
