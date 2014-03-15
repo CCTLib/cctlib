36ab2062d423a5ee76bfd1c11707090d532131e0
chabbi.milind <chabbi.milind@gmail.com>
2014-03-15 18:39:58 +0000

    Removed extra CXXFLAGS in the build.sh file

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ PATH_TO_PIN=/projects/hpc/mc29/software/pin_rev/pin-2.13-62732-gcc.4.4.7-linux/
 PATH_TO_BOOST=/projects/pkgs/boost_1_47_0/
 #develop is off by default
 #./configure --with-Pin=$PATH_TO_PIN --with-boost=$PATH_TO_BOOST --with-sparse-hash=$PATH_TO_GOOGLE_SPARSE_HASH --enable-develop
-./configure --with-Pin=$PATH_TO_PIN --with-boost=$PATH_TO_BOOST --with-sparse-hash=$PATH_TO_GOOGLE_SPARSE_HASH CXXFLAGS="-O2"
+./configure --with-Pin=$PATH_TO_PIN --with-boost=$PATH_TO_BOOST --with-sparse-hash=$PATH_TO_GOOGLE_SPARSE_HASH 
 make
 echo "*********YOU SUCCESSFULLY BUILT CCTLib***********"
 # uncomment to run sanity tests
