0715ff27717cf129a8e442159dbbcf2d5392480e
chabbi.milind@gmail.com <chabbi.milind@gmail.com@cdcf3bf0-21c1-d580-df87-83d91baef4e5>
2014-03-15 07:36:17 +0000

    added aclocal.m4 and config.h.in

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,5 +1,5 @@
 set -e
-PATH_TO_GOOGLE_SPARSE_HASH=/projects/hpc/mc29/software/sparsehash-2.0.2
+PATH_TO_GOOGLE_SPARSE_HASH=/projects/hpc/mc29/software/sparsehash-2.0.2-install/
 PATH_TO_PIN=/projects/hpc/mc29/software/pin_rev/pin-2.13-62732-gcc.4.4.7-linux/
 PATH_TO_BOOST=/projects/pkgs/boost_1_47_0/
 #develop is off by default
