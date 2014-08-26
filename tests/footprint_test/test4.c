7fb29aedc0931532498ff5e06933b94d8389e3ea
xu.liu06 <xu.liu06@gmail.com>
2014-08-26 13:11:43 +0000

    one more with control flow

---
+++ tests/footprint_test/test4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define N (10000000)
+int A[N];
+int B[N/2];
+int C[N];
+
+void g()
+{
+  int i=0;
+  for (i = 0; i<N; i++) {
+    if (i < N/2)
+      A[i] ++;
+    else
+      C[i] ++;
+  }
+}
+
+void h()
+{
+  int i;
+  for (i = 0; i<N/2; i++) B[i]++;
+}
+
+void f()
+{
+  g();
+}
+
+int main()
+{
+  f();
+  h();
+  return 0;
+}
