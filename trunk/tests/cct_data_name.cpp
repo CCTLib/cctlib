386a6be749d50e37e77ab001380bc22a17d99f03
shendu.nju@gmail.com <shendu.nju@gmail.com@cdcf3bf0-21c1-d580-df87-83d91baef4e5>
2015-04-08 01:04:00 +0000

    cct_data_name.cpp: new feature: data-thread view.

--- trunk/tests/cct_data_name.cpp
+++ trunk/tests/cct_data_name.cpp
@@ -48,7 +48,8 @@
 #include "pin.H"
 
 #define OFFSET 20000000
-#define ChunkSize 512*(1<<10)
+//#define MAX_WINDOW_ALLOWED 200
+//#define ChunkSize 512*(1<<10)
 //#define WINDOW 100000
 
 #define USE_TREE_BASED_FOR_DATA_CENTRIC
@@ -71,24 +72,29 @@ struct DataObj {
 	DataHandle_t data;
 } ;
 
-//struct AtomicCounter {
-//    std::atomic<long> value;
-//	void init() {value.store(0);}
-//    void increment(){++value;}
-//    long retrieve(){return value.load();}
-//};
 
-long CTR = 0;
-int WINDOW_WIDTH = 1000000;
+
+PIN_LOCK clientLock;
+
+
+
+long long CTR = 0;
+int WINDOW_WIDTH = 500*1000;
+int MAX_WINDOW_ALLOWED = 1000;
+int ChunkSize = 512*(1<<10);
+
 int window;
 int TH = 0;//thread to listen
 //unsigned long dataSizeTh = 1 << 10;//listen on data that is larger.
 bool outputflag = true;
+bool errflag = true;
 float OutputTH = 0.5; //threshold for output similar data objects.
-unordered_map <uint32_t, DataObj> dataObjectList;
+unordered_map <uint32_t, DataObj> DataObjectMap;
+unordered_map <uint32_t, long*> DataWindowMap;//(data ID, array(Windows))
+unordered_map <uint32_t, int*> DataThreadMap;//(data ID, array(Windows))
 
 //unordered_map <uint32_t, vector<int>> DataWindowMap;//(data ID, Vector(Windows))
-unordered_map <uint32_t, int*> DataWindowMap;//(data ID, Vector(Windows))
+
 
 
 
@@ -99,23 +105,28 @@ VOID ImageUnload(IMG img, VOID * v) {
 	outputflag = false;
 	int ii, jj;
 	fprintf(gTraceFile, "Unloading %s\n", IMG_Name(img).c_str());
-	printf("\n[CCTLIB] Unloading....\n");
-	printf("\nTotal accesses: %ld \n", CTR);
+	printf("\n [CCTLIB client] Unloading....");
+	printf("\n [CCTLIB client] Total accesses: ");
+	if (CTR>1000000)
+		printf("%d,%.3d,%.3d.\n\n", CTR/1000000, (CTR%1000000)/1000,CTR%1000);	
+	else
+		printf("%d,%.3d.\n\n", CTR/1000,CTR%1000);	
+
 
 	//sort
-	int length = dataObjectList.size();
+	int length = DataObjectMap.size();
 	uint32_t indexList[length], ind_tmp;
 	long counterList[length],temp;
 	float ratioList[length],ratio_temp;
 	DataObj tempData;
 
 	fprintf(gTraceFile,"\n\nA total of %ld memeroy accesses.\n",CTR);
-	fprintf(gTraceFile,"Window size: %ld .\n",WINDOW_WIDTH);
+	fprintf(gTraceFile,"Window Width: %ld .\n",WINDOW_WIDTH);
 	fprintf(gTraceFile,"total windows: %d .\n",window+1);
 
 
 	ii = 0;
-	for ( auto iter = dataObjectList.begin(); iter != dataObjectList.end(); ++iter )
+	for ( auto iter = DataObjectMap.begin(); iter != DataObjectMap.end(); ++iter )
 	{		
 		indexList[ii] = iter->first;
 		tempData = iter->second;
@@ -144,8 +155,8 @@ VOID ImageUnload(IMG img, VOID * v) {
 		uint32_t sysIndex;
 		int chunk = 0;
 
-		auto search = dataObjectList.find(index);
-		if(search != dataObjectList.end())
+		auto search = DataObjectMap.find(index);
+		if(search != DataObjectMap.end())
 		{
 			tempData = search->second;
 //			printf("1index = %d, 1synName=%d, chunk = %d\n", index, tempData.data.symName, chunk);
@@ -164,10 +175,10 @@ VOID ImageUnload(IMG img, VOID * v) {
 			int sizeInBytes = (int)tail-head;
 
 			if (tempData.data.objectType == DYNAMIC_OBJECT)
-				fprintf(gTraceFile,"\n\nRank %d -->> Dynamic Data %d chunk %d accessed %ld times, range:[%p,%p]\n",
+				fprintf(gTraceFile,"\n\n\nRank %d >>>> Dynamic Data %d chunk %d accessed %ld times, range:[%p,%p]\n",
 					ii,sysIndex, chunk, tempData.counter, head, tail);
 			else if(tempData.data.objectType == STATIC_OBJECT)
-				fprintf(gTraceFile,"\n\nRank %d -->> Static Data %d: \"%s\", accessed %ld times, range:[%p,%p]\n",
+				fprintf(gTraceFile,"\n\n\nRank %d >>>> Static Data %d: \"%s\", accessed %ld times, range:[%p,%p]\n",
 					ii,sysIndex, GetStringPool()+tempData.data.symName, tempData.counter, head, tail);
 
 			if (sizeInBytes > 1<<20)
@@ -177,30 +188,49 @@ VOID ImageUnload(IMG img, VOID * v) {
 			else 
 				fprintf(gTraceFile,"size = %d B.\n", sizeInBytes);
 
+			//data - window
 			auto search1 = DataWindowMap.find(index);
 			if(search1 != DataWindowMap.end())
 			{
-				int *window_arr = search1->second;
+				long *window_arr = search1->second;
 
-				fprintf(gTraceFile,"at windows: \t");
+				fprintf(gTraceFile,"distribtion among windows: \n");
 
-				for (jj=0;jj<window+1;jj++ )
+				for (jj=0;jj<min(window+1,MAX_WINDOW_ALLOWED);jj++ )
 				{
-					fprintf(gTraceFile,"%d\t",window_arr[jj]);
+					fprintf(gTraceFile,"|%ld\t",window_arr[jj]);
 				}
 				fprintf(gTraceFile,"\n");
 			}
 			else
+			{
 				fprintf(gTraceFile,"!!not here!!\n");
-			
+			}
 
+			//data - thread
+			auto search2 = DataThreadMap.find(index);
+			if(search2 != DataThreadMap.end())
+			{
+				int *th_arr = search2->second;
+				fprintf(gTraceFile,"accessed by threads: \n");
+				for (jj=0;jj<32;jj++ )
+				{
+					fprintf(gTraceFile,"|%d\t",th_arr[jj]);
+				}
+				fprintf(gTraceFile,"\n");
+			}
+			else
+			{
+				fprintf(gTraceFile,"!!not here!!\n");
+			}
+
+			
 			PrintFullCallingContext(sysIndex);
 //			printf("here6\n");
-
 		}
 	}
 
-    fprintf(gTraceFile,"\n\n\n End...");
+	fprintf(gTraceFile,"\n\n\n End...");
 }
 
 
@@ -218,7 +248,7 @@ void ClientInit(int argc, char* argv[]) {
     gethostname(name + strlen(name), MAX_FILE_PATH - strlen(name));
     pid_t pid = getpid();
     sprintf(name + strlen(name), "%d", pid);
-    cerr << "\n Creating log file at:" << name << "\n\n";
+    cerr << "\n [CCTLIB client] Creating log file at:" << name << "\n\n";
     gTraceFile = fopen(name, "w");
     // print the arguments passed
     fprintf(gTraceFile, "\n");
@@ -229,7 +259,7 @@ void ClientInit(int argc, char* argv[]) {
     fprintf(gTraceFile, "\n");
 	
 	// Register ImageUnload to be called when the image is unloaded
-    IMG_AddUnloadFunction(ImageUnload, 0);
+	IMG_AddUnloadFunction(ImageUnload, 0);
 }
 
 
@@ -238,7 +268,7 @@ VOID SimpleCCTQuery(THREADID id, const uint32_t slot) {
 }
 
 
-uint32_t getIndex(uint64_t beg, uint64_t end, void* addr, uint32_t sysIndex)
+uint32_t getIndex(uint64_t beg, uint64_t end, void* addr, uint32_t sysIndex, THREADID threadId)
 {
 	int size = (int)((end - beg)/1024);
 	int offset = (int)(((long)addr - beg)/1024);
@@ -246,7 +276,13 @@ uint32_t getIndex(uint64_t beg, uint64_t end, void* addr, uint32_t sysIndex)
 	int chunk  = floor(offset/(ChunkSize/1024)) + 1;
 	if (chunk>99)
 	{
-		printf("\n!!too large array!! Results may be iffy.\n\n");
+		if(errflag)
+		{
+			printf("\n [CCTLIB client] !!too large array!! Results of window distribution may be iffy.\n\n");
+			PIN_GetLock(&clientLock, threadId + 1);
+			errflag = false;
+			PIN_ReleaseLock(&clientLock);
+		}
 		chunk = chunk%100;
 	}
 
@@ -267,34 +303,61 @@ void updateDataList(void* addr, DataHandle_t data, THREADID threadId)
 	if (size < (800* (1<<10) ))
 		index = sysIndex;
 	else
-		index = getIndex(data.beg_addr, data.end_addr, addr, sysIndex);
+		index = getIndex(data.beg_addr, data.end_addr, addr, sysIndex, threadId);//divide into chunks
 
-	auto search = dataObjectList.find(index);
-	if(search != dataObjectList.end()) {
+	auto search = DataObjectMap.find(index);
+	if(search != DataObjectMap.end()) {
 		//update existing one:
 		__sync_fetch_and_add(&(search->second).counter,1);
 	}
-	else
-	{	//insert new one
+	else{//insert new one
 		DataObj newDataObj;
 		newDataObj.counter = 1;
 		newDataObj.data = data;
-		dataObjectList.insert({index,newDataObj});
+		PIN_GetLock(&clientLock, threadId + 1);
+		DataObjectMap.insert({index,newDataObj});
+		PIN_ReleaseLock(&clientLock);
 	}
+	
 
-
+ // TODO: dynamically ajust window width
 	auto search1 = DataWindowMap.find(index);
 	if(search1 != DataWindowMap.end()) {
-		//update existing one:
-		
-		int *window_arr = search1->second;
-		__sync_fetch_and_add(&(window_arr[window]),1);		
+		if (window < MAX_WINDOW_ALLOWED)
+		{	//update existing one:		
+			long *window_arr = search1->second;
+			__sync_fetch_and_add(&(window_arr[window]),1);
+		}
 	}
-	else
-	{	//insert new one
-		int *win_arr = new int[200];
+	else {//insert new one
+		long *win_arr = new long[MAX_WINDOW_ALLOWED];
+		for (int ii=0; ii<MAX_WINDOW_ALLOWED;ii++ )
+			win_arr[ii]=0;
+		PIN_GetLock(&clientLock, threadId + 1);
 		DataWindowMap.insert({index, win_arr});
+		PIN_ReleaseLock(&clientLock);
 	}
+
+
+	auto search2 = DataThreadMap.find(index);
+	if(search2 != DataThreadMap.end()) {
+		if (threadId < 32)
+		{	//update existing one:		
+			int *th_arr = search2->second;
+			//if (th_arr[threadId]==0)
+				__sync_fetch_and_add(&(th_arr[threadId]),1);
+		}
+	}
+	else {//insert new one
+		int *th_arr = new int[32];
+		for (int ii=0; ii<32;ii++ )
+			th_arr[ii]=0;
+		PIN_GetLock(&clientLock, threadId + 1);
+		DataThreadMap.insert({index, th_arr});
+		PIN_ReleaseLock(&clientLock);
+
+	}
+	
 }
 
 
@@ -304,7 +367,11 @@ VOID MemAnalysisRoutine(void* addr, THREADID threadId)
 	__sync_fetch_and_add(&CTR,1);
 
 	if ((window+1)*WINDOW_WIDTH < CTR)
-		window = CTR/WINDOW_WIDTH;	
+	{
+		PIN_GetLock(&clientLock, threadId + 1);
+		window = CTR/WINDOW_WIDTH;
+		PIN_ReleaseLock(&clientLock);
+	}
 
 	DataHandle_t d = GetDataObjectHandle(addr, threadId);
 	
