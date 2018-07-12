02e81412264cfe23b34577360544db90583a040d
Milind Chabbi <chabbimilind@gmail.com>
2018-07-12 02:37:57 -0400

    Generic memory block size handling to generalize cacheline, os page, or any blocking factor

--- clients/ins_reuse_client.cpp
+++ clients/ins_reuse_client.cpp
@@ -21,6 +21,8 @@
 #include <algorithm>
 #include <list>
 #include <bits/stdc++.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 #include "pin.H"
 #include "cctlib.H"
@@ -28,6 +30,8 @@
 #include "rbtree.h"
 #include <xmmintrin.h>
 #include <immintrin.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 extern "C" {
 #include "xed-interface.h"
@@ -39,6 +43,8 @@ extern "C" {
 using google::sparse_hash_map;  // namespace where class lives by default
 using google::dense_hash_map;
 
+// Short alias for this namespace
+namespace pt = boost::property_tree;
 using namespace std;
 using namespace PinCCTLib;
 
@@ -49,7 +55,10 @@ using namespace PinCCTLib;
 #define CACHELINE_SIZE (1L<<CACHELINE_BIT)
 #define CACHELINE_MASK (CACHELINE_SIZE-1)
 
-#define GET_CACHELINE(addr) (((uint64_t)addr) & (~(CACHELINE_MASK)))
+#define GET_CACHELINE(addr) (((size_t)addr) & (~(CACHELINE_MASK)))
+#define GET_CACHELINE(addr) (((size_t)addr) & (~(CACHELINE_MASK)))
+
+#define NUM_BLOCKS (2)
 
 #define FIRST_USE (ULLONG_MAX)
 
@@ -67,14 +76,22 @@ struct InsReuseThreadData{
     char padding1[128];
     uint64_t bytesLoad;
     long long numIns;
-    RBTree_t InsRBTree;
-    RBTree_t CLRBTree;
+    RBTree_t insRBTree;
+    RBTree_t clRBTree;
     uint64_t numInsExecuted;
     uint64_t numCacheLines;
     uint64_t insReuseHisto[MAX_REUSE_DISTANCE_BINS];
     uint64_t clReuseHisto[MAX_REUSE_DISTANCE_BINS];
     ShadowMemory<uint64_t> smIns;
     ShadowMemory<uint64_t> smCL;
+    
+    struct{
+        RBTree_t rbTree;
+        uint64_t numBlocksCounter;
+        uint64_t reuseHisto[MAX_REUSE_DISTANCE_BINS];
+        ShadowMemory<uint64_t> sm;
+    } blockData[NUM_BLOCKS];
+    
     bool sampleFlag;
     char padding2[128];
 };
@@ -94,6 +111,15 @@ inline InsReuseThreadData* ClientGetTLS(const THREADID threadId) {
 #endif
 }
 
+static inline size_t GetCacheline(size_t addr) {
+    return addr & ~(CACHELINE_MASK);
+}
+
+static inline size_t GetBlock(size_t addr, size_t blockMask) {
+    return addr & (blockMask);
+}
+
+
 static INT32 Usage() {
     PIN_ERROR("Pin tool to profile instruction reuse distance.\n" + KNOB_BASE::StringKnobSummary() + "\n");
     return -1;
@@ -101,6 +127,10 @@ static INT32 Usage() {
 
 // Main for RedSpy, initialize the tool, register instrumentation functions and call the target program.
 static FILE* gTraceFile;
+static ofstream gJSONFile;
+
+// Create a pt root
+static pt::ptree ptTree;
 
 // Initialized the needed data structures before launching the target program
 static void ClientInit(int argc, char* argv[]) {
@@ -112,6 +142,7 @@ static void ClientInit(int argc, char* argv[]) {
         // assumes max of MAX_FILE_PATH
         strcpy(name, envPath);
     }
+    
     
     gethostname(name + strlen(name), MAX_FILE_PATH - strlen(name));
     pid_t pid = getpid();
@@ -127,6 +158,24 @@ static void ClientInit(int argc, char* argv[]) {
     
     fprintf(gTraceFile, "\n");
     
+    int i = 0;
+    for(i = 0 ; i < argc; i++) {
+        if (0 == strcmp(argv[i], "--")) {
+            i++;
+            break;
+        }
+    }
+    if (i < argc) {
+        ptTree.put("exe", argv[i]);
+        i++;
+    }
+    stringstream ss;
+    for(; i < argc; i++) {
+        ss << argv[i] <<" ";
+    }
+    ptTree.put("args", ss.str());
+    gJSONFile.open(string(name) + ".json");
+    
     // Init Xed
     // Init XED for decoding instructions
     xed_state_init(&InsReuseGlobals.xedState, XED_MACHINE_MODE_LONG_64, (xed_address_width_enum_t) 0, XED_ADDRESS_WIDTH_64b);
@@ -137,7 +186,7 @@ static inline void UpdateInsReuseStats(uint64_t distance, uint64_t count, InsReu
     
     // Bin 0: [0, 1)
     // Bin 1  [1, 2)
-    // Bin 3  [2, 4)
+    // Bin 2  [2, 4)
     // Bin 3  [4, 8)
     
     if (distance >= MAX_REUSE_DISTANCE) {
@@ -153,7 +202,7 @@ static inline void UpdateInsReuseStats(uint64_t distance, uint64_t count, InsReu
 static inline void UpdateCacheLineReuseStats(uint64_t distance, uint64_t count, InsReuseThreadData* tData){
     // Bin 0: [0, 1)
     // Bin 1  [1, 2)
-    // Bin 3  [2, 4)
+    // Bin 2  [2, 4)
     // Bin 3  [4, 8)
     
     uint64_t bin;
@@ -168,6 +217,27 @@ static inline void UpdateCacheLineReuseStats(uint64_t distance, uint64_t count, 
     tData->clReuseHisto[bin] ++;
     // The rest of the instructions on the same cacheline have a zero distance
     tData->clReuseHisto[0] += count-1;
+}
+
+
+static inline void UpdateBlockReuseStats(uint64_t distance, uint64_t count, uint64_t* reuseHisto){
+    // Bin 0: [0, 1)
+    // Bin 1  [1, 2)
+    // Bin 2  [2, 4)
+    // Bin 3  [4, 8)
+    
+    uint64_t bin;
+    if (distance >= MAX_REUSE_DISTANCE) {
+        bin = MAX_REUSE_DISTANCE_BINS-1;
+    } else if (distance == 0) {
+        bin = 0;
+    } else {
+        bin = (sizeof(uint64_t)*8 - 1 - __builtin_clzl(distance)) + 1 /* 1 for dedicated bin 0*/;
+    }
+    // One instruction is at this distance
+    reuseHisto[bin] ++;
+    // The rest of the instructions on the same cacheline have a zero distance
+    reuseHisto[0] += count-1;
 }
 
 static inline uint64_t ComputeInsReuseDistance(uint64_t prevTick, uint64_t newTick, uint32_t v, InsReuseThreadData * tData, RBTree_t * rbt){
@@ -206,6 +276,23 @@ static inline uint64_t ComputeCLReuseDistance(uint64_t prevTick, uint64_t newTic
     }
 }
 
+static inline uint64_t ComputeBlockReuseDistance(uint64_t prevTick, uint64_t newTick, uint32_t v, InsReuseThreadData * tData, RBTree_t * rbt){
+    // Find how many RB-Tree nodes are to the right of this node.
+    uint64_t reuseDist;
+    auto node = rbt->FindSumGreaterThan(prevTick, &reuseDist);
+    if (node) {
+        //  Delete the node from RB-tree
+        auto retNode = rbt->Delete(node);
+        // reinsert the node with new tick
+        retNode->key =  newTick;
+        retNode->value =  v;
+        rbt->Insert(retNode);
+        return reuseDist;
+    } else {
+        return FIRST_USE;
+    }
+}
+
 
 static inline void AnalyzeInsLevelReuse(void * insAddr, uint32_t numInsInBBL,  THREADID threadId){
     assert(0 != numInsInBBL);
@@ -222,9 +309,9 @@ static inline void AnalyzeInsLevelReuse(void * insAddr, uint32_t numInsInBBL,  T
         // no update needed
         // However, needs a new insertion
         auto newNode = new TreeNode<uint64_t, uint32_t, uint64_t>(tData->numInsExecuted, numInsInBBL);
-        tData->InsRBTree.Insert(newNode);
+        tData->insRBTree.Insert(newNode);
     } else {
-        uint64_t reuseDistance = ComputeInsReuseDistance(prevTick,  tData->numInsExecuted, numInsInBBL, tData, & tData->InsRBTree);
+        uint64_t reuseDistance = ComputeInsReuseDistance(prevTick,  tData->numInsExecuted, numInsInBBL, tData, & tData->insRBTree);
         assert(FIRST_USE != reuseDistance);
         UpdateInsReuseStats(reuseDistance, numInsInBBL, tData);
     }
@@ -242,14 +329,17 @@ static inline void AnalyzeCacheLineLevelReuse(void * cacheLine, uint32_t numInsI
     if (prevTick == 0 /* first use */) {
         // needs a new insertion
         auto newNode = new TreeNode<uint64_t, uint32_t, uint64_t>(tData->numCacheLines, numInsInCacheLine);
-        tData->CLRBTree.Insert(newNode);
+        tData->clRBTree.Insert(newNode);
         
         if (numInsInCacheLine > 1) {
             // The rest of the instructions on the same cacheline have a zero distance
             tData->clReuseHisto[0] += numInsInCacheLine-1;
         }
     } else {
-        uint64_t reuseDistance = ComputeCLReuseDistance(prevTick, tData->numCacheLines, numInsInCacheLine, tData, & tData->CLRBTree);
+        // TODO: Optimization: if the last cacheline access is same as this one, we can bypass this RB-Tree deletion, insertion
+        // and instead directly increment the key. Need to be careful not to violate any tree properties.
+        // May have to check to make sure that the node value is also unchanged.
+        uint64_t reuseDistance = ComputeCLReuseDistance(prevTick, tData->numCacheLines, numInsInCacheLine, tData, & tData->clRBTree);
         assert(FIRST_USE != reuseDistance);
         UpdateCacheLineReuseStats(reuseDistance, numInsInCacheLine, tData);
     }
@@ -258,9 +348,45 @@ static inline void AnalyzeCacheLineLevelReuse(void * cacheLine, uint32_t numInsI
     tData->numCacheLines++;
 }
 
+template <int blockSize, int blkIdx>
+static inline void AnalyzeBlockLevelReuse(void * block, uint32_t numInsInBlock,  THREADID threadId){
+    assert(0 != numInsInBlock);
+    // blockSize must be a power of 2.
+    assert(1 == __builtin_popcountll(blockSize));
+    const size_t blockBits = __builtin_ctzll(blockSize);
+    const size_t blockMask = ~(blockSize-1);
+    
+    InsReuseThreadData* tData = ClientGetTLS(threadId);
+    auto blockData = & (tData->blockData[blkIdx]);
+    tuple<uint64_t[SHADOW_PAGE_SIZE]> &t = blockData->sm.GetOrCreateShadowBaseAddress((size_t)block);
+    uint64_t * shadowMemAddr = &(get<0>(t)[PAGE_OFFSET((size_t)block)]);
+    uint64_t prevTick = *shadowMemAddr;
+    
+    if (prevTick == 0 /* first use */) {
+        // needs a new insertion
+        auto newNode = new TreeNode<uint64_t, uint32_t, uint64_t>(blockData->numBlocksCounter, numInsInBlock);
+        blockData->rbTree.Insert(newNode);
+        
+        if (numInsInBlock > 1) {
+            // The rest of the instructions on the same cacheline have a zero distance
+            blockData->reuseHisto[0] += numInsInBlock-1;
+        }
+    } else {
+        // TODO: Optimization: if the last block access is same as this one, we can bypass this RB-Tree deletion, insertion
+        // and instead directly increment the key. Need to be careful not to violate any tree properties.
+        // May have to check to make sure that the node value is also unchanged.
+        uint64_t reuseDistance = ComputeBlockReuseDistance(prevTick, blockData->numBlocksCounter, numInsInBlock, tData, & blockData->rbTree);
+        assert(FIRST_USE != reuseDistance);
+        UpdateBlockReuseStats(reuseDistance, numInsInBlock, blockData->reuseHisto);
+    }
+    *shadowMemAddr = blockData->numBlocksCounter;
+    // Update the tick
+    blockData->numBlocksCounter++;
+}
 
-//instrument the trace, count the number of ins in the trace and instrument each BBL
-static void InstrumentTrace(TRACE trace, void* f) {
+
+static void InstrumentInsLevelReuse(TRACE trace, void* f) {
+    // Instruction-level reuse distance
     for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)){
         uint32_t totInsInBbl = BBL_NumIns(bbl);
         ADDRINT insAddr = BBL_Address(bbl);
@@ -270,14 +396,17 @@ static void InstrumentTrace(TRACE trace, void* f) {
                        IARG_UINT32, totInsInBbl,
                        IARG_THREAD_ID, IARG_END);
     }
-    
+}
+
+static void InstrumentCLLevelReuse(TRACE trace, void* f) {
+    // Cacheline-level reuse distance
     for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)){
         uint32_t numInsInCacheLine = 0;
-        uint64_t prevCacheLine = GET_CACHELINE(INS_Address(BBL_InsHead(bbl)));
+        uint64_t prevCacheLine = GetCacheline( (uint64_t) INS_Address(BBL_InsHead(bbl)));
         for(INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins=INS_Next(ins)) {
             // Does instruction straddle two cachelines?
-            uint64_t insStartCacheLine = GET_CACHELINE(INS_Address(ins));
-            uint64_t insEndCacheLine = GET_CACHELINE(INS_Address(ins) + INS_Size(ins) - 1);
+            uint64_t insStartCacheLine = GetCacheline((uint64_t) INS_Address(ins));
+            uint64_t insEndCacheLine = GetCacheline((uint64_t) (INS_Address(ins) + INS_Size(ins) - 1));
             
             // +1 for the ongoing cacheline
             if(insStartCacheLine == prevCacheLine) {
@@ -315,6 +444,60 @@ static void InstrumentTrace(TRACE trace, void* f) {
     }
 }
 
+template <int blockSize, int blkIdx>
+static void InstrumentMemBlockLevelReuse(TRACE trace, void* f) {
+    // blockSize must be a power of 2.
+    assert(1 == __builtin_popcountll(blockSize));
+    const size_t blockBits = __builtin_ctzll(blockSize);
+    const size_t blockMask = ~(blockSize-1);
+    
+    // Cacheline-level reuse distance
+    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)){
+        uint32_t numInsInMemBlock = 0;
+        size_t prevMemBlock = GetBlock((uint64_t) INS_Address(BBL_InsHead(bbl)), blockMask);
+        for(INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins=INS_Next(ins)) {
+            // Does instruction straddle two mem blocks?
+            size_t insStartMemBlock = GetBlock((size_t) INS_Address(ins), blockMask);
+            size_t insEndMemBlock = GetBlock((size_t) (INS_Address(ins) + INS_Size(ins) - 1), blockMask);
+            
+            // +1 for the ongoing mem block
+            if(insStartMemBlock == prevMemBlock) {
+                numInsInMemBlock ++;
+            }
+            
+            // Assumption: ins can never span more than one memory block.
+            // Continue adding more to this mem block
+            if (insEndMemBlock == prevMemBlock) {
+                continue;
+            }
+            // either the ins ends in a new block or begins on a new block.
+            
+            INS_InsertCall(ins,
+                           IPOINT_BEFORE,(AFUNPTR)AnalyzeBlockLevelReuse<blockSize, blkIdx>,
+                           IARG_ADDRINT, prevMemBlock,
+                           IARG_UINT32, numInsInMemBlock,
+                           IARG_THREAD_ID, IARG_END);
+            numInsInMemBlock = 1;
+            prevMemBlock = insEndMemBlock;
+        }
+        
+        INS_InsertCall(BBL_InsTail(bbl),
+                       IPOINT_BEFORE, (AFUNPTR)AnalyzeBlockLevelReuse<blockSize, blkIdx>,
+                       IARG_ADDRINT, prevMemBlock,
+                       IARG_UINT32, numInsInMemBlock,
+                       IARG_THREAD_ID, IARG_END);
+        
+    }
+}
+
+//instrument the trace, count the number of ins in the trace and instrument each BBL
+static void InstrumentTrace(TRACE trace, void* f) {
+    InstrumentInsLevelReuse(trace, f);
+    InstrumentCLLevelReuse(trace, f);
+    InstrumentMemBlockLevelReuse<CACHELINE_SIZE, 0>(trace, f);
+    InstrumentMemBlockLevelReuse<4096, 1>(trace, f);
+}
+
 // On each Unload of a loaded image, the accummulated redundancy information is dumped
 static VOID ImageUnload(IMG img, VOID* v) {
     fprintf(gTraceFile, "\n TODO .. Multi-threading is not well supported.");
@@ -327,10 +510,16 @@ static VOID ImageUnload(IMG img, VOID* v) {
 static VOID ThreadFiniFunc(THREADID threadid, const CONTEXT *ctxt, INT32 code, VOID *v) {
 }
 
-static void DumpHisto(uint64_t * histo){
+static void DumpHisto(uint64_t * histo, string key){
     for(int i = 0; i < MAX_REUSE_DISTANCE_BINS; i++) {
         fprintf(gTraceFile, "\n %d %lu", i, histo[i]);
     }
+    
+    pt::ptree subNode;
+    for(int i = 0; i < MAX_REUSE_DISTANCE_BINS; i++) {
+        subNode.put(to_string(i), histo[i]);
+    }
+    ptTree.add_child(key, subNode);
 }
 
 /*
@@ -343,14 +532,30 @@ static void DumpHisto(uint64_t * histo){
  */
 
 static VOID FiniFunc(INT32 code, VOID *v) {
+    
+    // Peak memory
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+    size_t peakRSS =  (size_t)(rusage.ru_maxrss);
+    struct timeval ut = rusage.ru_utime;
+    struct timeval st = rusage.ru_stime;
+    
+    
     THREADID  threadId =  PIN_ThreadId();
     InsReuseThreadData* tData = ClientGetTLS(threadId);
     fprintf(gTraceFile, "\nInstruction-reuse histo");
-    DumpHisto(tData->insReuseHisto);
+    DumpHisto(tData->insReuseHisto, "InsReuse");
     fprintf(gTraceFile, "\nCL-reuse histo");
-    DumpHisto(tData->clReuseHisto);
+    DumpHisto(tData->clReuseHisto, "CacheLineReuse");
+    DumpHisto(tData->blockData[0].reuseHisto, "CacheLineReuse2");
+    DumpHisto(tData->blockData[1].reuseHisto, "CacheLineReuse2");
     
+    ptTree.put("utime", to_string(ut.tv_sec * 1000000 + ut.tv_usec));
+    ptTree.put("stime", to_string(st.tv_sec * 1000000 + st.tv_usec));
+    ptTree.put("RSS", to_string(peakRSS));
     
+    pt::write_json(gJSONFile, ptTree);
+    gJSONFile.close();
     fclose(gTraceFile);
 }
 
